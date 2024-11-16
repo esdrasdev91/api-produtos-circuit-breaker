6284a617fdb297c735d4acf943876f777e839d53
Esdras Pinheiro <esdras.dev@hotmail.com>
2024-11-16 10:40:55 -0300

    First commit

---
+++ src/main/java/com/esdrasdev/api_produtos_circuit_breaker/ApiProdutosCircuitBreakerApplication.java
@@ -0,0 +1,13 @@
+package com.esdrasdev.api_produtos_circuit_breaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiProdutosCircuitBreakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiProdutosCircuitBreakerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/esdrasdev/api_produtos_circuit_breaker/ApiProdutosCircuitBreakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esdrasdev.api_produtos_circuit_breaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiProdutosCircuitBreakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
