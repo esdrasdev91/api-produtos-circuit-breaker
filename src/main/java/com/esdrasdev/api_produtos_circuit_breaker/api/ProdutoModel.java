08b33897cee73c5afcf8be476eb95c1ca5b93e91
Esdras Pinheiro <esdras.dev@hotmail.com>
2024-11-16 10:47:51 -0300

    create class model

---
+++ src/main/java/com/esdrasdev/api_produtos_circuit_breaker/api/ProdutoModel.java
@@ -0,0 +1,65 @@
+package com.esdrasdev.api_produtos_circuit_breaker.api;
+
+public class ProdutoModel {
+
+    public Long id;
+    public String nome;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    public List<AvaliacaoModel> avaliacoes;
+
+    public ProdutoModel() {
+
+    }
+
+    public ProdutoModel(Long id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public ProdutoModel(Long id, String nome, List<AvaliacaoModel> avaliacoes) {
+        this.id = id;
+        this.nome = nome;
+        this.avaliacoes = avaliacoes;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<AvaliacaoModel> getAvaliacoes() {
+        return avaliacoes;
+    }
+
+    public void setAvaliacoes(List<AvaliacaoModel> avaliacoes) {
+        this.avaliacoes = avaliacoes;
+    }
+
+    public static ProdutoModel of(Produto produto) {
+        return new ProdutoModel(
+                produto.getId(),
+                produto.getNome()
+        );
+    }
+
+    public static ProdutoModel of(Produto produto, List<AvaliacaoModel> avaliacoes) {
+        return new ProdutoModel(
+                produto.getId(),
+                produto.getNome(),
+                avaliacoes
+        );
+    }
+
+}
