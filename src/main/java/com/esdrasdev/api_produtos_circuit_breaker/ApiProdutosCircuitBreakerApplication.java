b637de167739e2270900f51688d49a83885698ef
esdrasdev91 <esdras.dev@hotmail.com>
2024-11-22 12:05:50 -0300

    Ajusts endpoint

--- src/main/java/com/esdrasdev/api_produtos_circuit_breaker/ApiProdutosCircuitBreakerApplication.java
+++ src/main/java/com/esdrasdev/api_produtos_circuit_breaker/ApiProdutosCircuitBreakerApplication.java
@@ -2,8 +2,10 @@ package com.esdrasdev.api_produtos_circuit_breaker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+//@EnableFeignClients
 public class ApiProdutosCircuitBreakerApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/esdrasdev/api_produtos_circuit_breaker/client/avaliacoes/AvaliacaoClient.java
+++ src/main/java/com/esdrasdev/api_produtos_circuit_breaker/client/avaliacoes/AvaliacaoClient.java
@@ -1,10 +1,12 @@
 package com.esdrasdev.api_produtos_circuit_breaker.client.avaliacoes;
 
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
+//@FeignClient(name = "avaliacaoClient", url = "http://localhost:8090")
 public interface AvaliacaoClient {
 
     List<AvaliacaoModel> buscarTodosPorProduto(Long productId);
