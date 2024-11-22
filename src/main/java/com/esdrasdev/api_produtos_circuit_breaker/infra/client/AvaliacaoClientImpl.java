996809a147762ff0481a68544b437ca67f78aa0a
Esdras Pinheiro <esdras.dev@hotmail.com>
2024-11-22 09:38:34 -0300

    Implements methods

--- src/main/java/com/esdrasdev/api_produtos_circuit_breaker/infra/client/AvaliacaoClientImpl.java
+++ src/main/java/com/esdrasdev/api_produtos_circuit_breaker/infra/client/AvaliacaoClientImpl.java
@@ -3,15 +3,52 @@ package com.esdrasdev.api_produtos_circuit_breaker.infra.client;
 import com.esdrasdev.api_produtos_circuit_breaker.client.avaliacoes.AvaliacaoClient;
 import com.esdrasdev.api_produtos_circuit_breaker.client.avaliacoes.AvaliacaoModel;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class AvaliacaoClientImpl implements AvaliacaoClient {
 
+    private final Logger logger = LoggerFactory.getLogger(AvaliacaoClientImpl.class);
+    private final RestTemplate restTemplate;
 
-    @Override
-    public List<AvaliacaoModel> buscarTodosPorProduto(Long productId) {
-        return List.of();
+    private final static String API_URL = UriComponentsBuilder
+            .fromHttpUrl("http://localhost:8090/avaliacaos")
+            .queryParam("produtoId", "{produtoId}")
+            .encode()
+            .toUriString();
+
+    public AvaliacaoClientImpl(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
     }
 
+    @Override
+    public List<AvaliacaoModel> buscarTodosPorProduto(Long produtoId) {
+        final List<AvaliacaoModel> avaliacoes = executarRequisicao(produtoId);
+        return avaliacoes;
+    }
+
+    private List<AvaliacaoModel> executarRequisicao(Long produtoId) {
+        final Map<String, Object> parametros = new HashMap<>();
+        parametros.put("produtoId", produtoId);
+
+        logger.info("Buscando avaliações");
+        final AvaliacaoModel[] avaliacoes;
+
+        try {
+            avaliacoes = restTemplate.getForObject(API_URL, AvaliacaoModel[].class, parametros);
+        } catch (Exception e) {
+            logger.error("Erro ao buscar avaliações");
+            throw e;
+        }
+
+        return Arrays.asList(avaliacoes);
+    }
 
 }
